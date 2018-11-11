365f18aa4ea00edc9d8de238e3406c8540a5b195
cmopbeatz.com <cmo_p@outlook.com>
2018-11-11 13:58:07 -0800

    update-11/10

---
+++ Assets/BtnTxtColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnTxtColor : MonoBehaviour {
+
+	private TextMeshPro mm_Text;
+
+
+	// Use this for initialization
+	void Awake () {
+		TextMeshPro mm_Text = GetComponent<TextMeshPro>();
+		mm_Text.faceColor = Color.white;
+	}
+	
+
+	void clickedTxt(){
+		mm_Text.faceColor = Color.white;
+	}
+
+	void outTxt(){
+		mm_Text.faceColor = Color.red;
+	}
+
+
+}
